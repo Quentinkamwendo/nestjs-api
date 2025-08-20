49f4af6668531a38aed303974bd8580098fdfa79
quentin kamwendo <quentinkamwendo673@gmail.com>
2025-08-20 08:27:14 +0200

    env changed

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,11 +8,11 @@ export const getDataSource = async () => {
   if (!dataSource) {
     dataSource = new DataSource({
       type: 'mysql',
-      host: process.env.HOST || 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       port: +process.env.DB_PORT || 3306,
-      username: process.env.USERNAME || 'root',
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [User, Items],
       synchronize: false,
       ssl: {
